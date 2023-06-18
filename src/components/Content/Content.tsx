d9bf22fed94623362d08ee6d6298d85f159fb8c6
MalcolmDahling <mac.dahling@gmail.com>
2023-06-18 16:09:11 +0200

    fixed stuff

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -13,6 +13,7 @@ const P = styled('p', {
 
     '@tablet':{
         padding:'$1',
+        minHeight:350,
     },
 
     variants:{
@@ -36,32 +37,54 @@ export default function Content(){
     const [enableAnim, setEnableAnim] = useState(false);
 
     const content = [
-        <span>Jag är en uppfinningsrik och kreativ person som lär sig snabbt och gärna tar mig an utmanande uppgifter där man får tänka lite.
-        Mitt arbete kan ske efter instruktion eller med större frihet i uppgifter där det krävs.
-        Jag kan jobba självständigt eller i grupp och tar gärna emot konstruktiv kritik för att förbättra mitt arbete.
-        Problemlösning är även det en av mina starka sidor och jag hoppar gärna in och hjälper andra vid behov.
-        I framtiden vill jag gärna se mig själv i en roll inom teknik eller programmering och webbutveckling.</span>,
+        //PROFIL
+        <span>
+            Jag är en uppfinningsrik och kreativ person som lär sig snabbt och gärna tar mig an utmanande uppgifter där man får tänka lite.
+            Mitt arbete kan ske efter instruktion eller med större frihet i uppgifter där det krävs.
+            Jag kan jobba självständigt eller i grupp och tar gärna emot konstruktiv kritik för att förbättra mitt arbete.
+            Problemlösning är även det en av mina starka sidor och jag hoppar gärna in och hjälper andra vid behov.
+            I framtiden vill jag gärna se mig själv i en roll inom programmering och webbutveckling.
+        </span>,
 
-        <span>Jag har en Frontend Developer-examen från Medieinstitutet Yrkeshögskola.
-        Kurser som ingår i programmet: HTML/CSS, 20 yhp. Javascript, grundkurs, 40 yhp. Javascript, fördjupning, 60 yhp.
-        Kompetensportfölj och entreprenörskap, 10 yhp. Arbetsmetodik för utvecklare, 20 yhp. Arbeta med projekt i agila metoder, 15 yhp.
-        Dynamisk webbutveckling, 45 yhp. Grafiska verktyg för gränssnittsdesign, 15 yhp. LIA 1, 50 yhp. LIA 2, 80 yhp. UX/Usability, 15 yhp. Examensarbete, 30 yhp.</span>,
+        //UTBILDNING
+        <span>
+            Jag har en Frontend Developer-examen från Medieinstitutet Yrkeshögskola.
+            Kurser som ingår i programmet: HTML/CSS, 20 yhp. Javascript, grundkurs, 40 yhp. Javascript, fördjupning, 60 yhp.
+            Kompetensportfölj och entreprenörskap, 10 yhp. Arbetsmetodik för utvecklare, 20 yhp. Arbeta med projekt i agila metoder, 15 yhp.
+            Dynamisk webbutveckling, 45 yhp. Grafiska verktyg för gränssnittsdesign, 15 yhp. LIA 1, 50 yhp. LIA 2, 80 yhp. UX/Usability, 15 yhp. Examensarbete, 30 yhp.
+        </span>,
 
-        <span>Kunnig inom HTML, CSS och Javascript (vanilla, React, Angular, Typescript och Node), även grundläggande kunskaper inom C#, C++, PHP och SQL.</span>,
+        //PROGRAMMERING
+        <span>
+            Kunnig inom HTML, CSS och Javascript (vanilla, React, Angular, Typescript och Node), även grundläggande kunskaper inom C#, C++, PHP och SQL.
+        </span>,
 
-        <span>Mina ansvarsområden har varit allt ifrån programmering, grafisk design, webbutveckling och lödning till mer praktiska uppgifter som montering av 
-        kontorsmöbler och installation av elektrisk utrustning och datorer.
-        Jag har också varigt med och skapat en utställning på Sydostasiatiskamuséet där jag ansvarade för den tekniska delen med TV-apparater, datorer och nätverk.</span>,
+        //KOMPETENSOMRÅDEN
+        <span>
+            Mina ansvarsområden har varit allt ifrån programmering, grafisk design, webbutveckling och lödning till mer praktiska uppgifter som montering av 
+            kontorsmöbler och installation av elektrisk utrustning och datorer.
+            Jag har också varigt med och skapat en utställning på Sydostasiatiskamuséet där jag ansvarade för den tekniska delen med TV-apparater, datorer och nätverk.
+        </span>,
 
-        <span>Lödare och programmerare - LATAB<br/>
-        Installatör och programmerare - Service Databörsen AB<br/>
-        Mångsysslare - Spånga Badminton och Bollcenter</span>,
+        //PRAKTIK
+        <span>
+            Lödare och programmerare - LATAB<br/>
+            Installatör och programmerare - Service Databörsen AB<br/>
+            Mångsysslare - Spånga Badminton och Bollcenter<br/>
+            Kruso - Frontend-utvecklare.
+        </span>,
 
-        <span>Svenska som modersmål.<br/>
-        Engelska, flytande.</span>,
+        //SPRÅKKUNSKAPER
+        <span>
+            Svenska som modersmål.<br/>
+            Engelska, flytande.
+        </span>,
 
-        <span>B-körkort.<br/>
-        På fritiden så gillar jag att styrketräna, cykla, och att spela biljard, schack och datorspel.</span>
+        //ÖVRIGT
+        <span>
+            B-körkort.<br/>
+            På fritiden så gillar jag att styrketräna, cykla, och att spela biljard, schack och datorspel.
+        </span>
     ];
 
     useEffect(() => {
