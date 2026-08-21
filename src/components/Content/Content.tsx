d91759ab43f4a4f21b7b86bfb97157f46a35536e
Malcolm Dahling <mac.dahling@gmail.com>
2026-08-21 15:52:56 +0200

    updated content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -97,12 +97,7 @@ export default function Content() {
     <span>Jag är en uppfinningsrik och kreativ person som lär sig snabbt och gärna tar mig an utmanande uppgifter där man får tänka lite. Mitt arbete kan ske efter instruktion eller med större frihet i uppgifter där det krävs. Jag kan jobba självständigt eller i grupp och tar gärna emot konstruktiv kritik för att förbättra mitt arbete. Problemlösning är även det en av mina starka sidor och jag hoppar gärna in och hjälper andra vid behov. I framtiden vill jag gärna se mig själv i en roll inom programmering och webbutveckling.</span>,
 
     //PROGRAMMERING
-    <span>
-      HTML, CSS, Javascript, Typescript, NodeJS, React, NextJS, JQuery, Angular, MongoDB, Sass, Git.
-      <br />
-      <br />
-      Har även grundläggande kunskaper inom C#, C++, PHP och SQL.
-    </span>,
+    <span>HTML, CSS, Javascript, Typescript, NodeJS, React, NextJS, JQuery, Angular, MongoDB, Sass, Git.</span>,
 
     //KOMPETENSOMRÅDEN
     <span>Mina ansvarsområden har varit allt ifrån programmering, grafisk design, webbutveckling och lödning till mer praktiska uppgifter som montering av kontorsmöbler och installation av elektrisk utrustning och datorer. Jag har också varit med och skapat en utställning på Sydostasiatiskamuséet där jag ansvarade för den tekniska delen med TV-apparater, datorer och nätverk.</span>,
