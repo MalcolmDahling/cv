c2c81edfeed10b7438ae185bbf6b2a3f25351fa7
Malcolm Dahling <mac.dahling@gmail.com>
2026-08-23 23:45:13 +0200

    changed animation delays

--- src/variables/animationDelays.ts
+++ src/variables/animationDelays.ts
@@ -1,43 +1,43 @@
 export const AnimationDelays = {
   //name
   name: 5500,
-  title: 7000,
-  email: 9200,
+  title: 7200,
+  email: 9700,
 
   //image
-  image: 9700,
+  image: 10300,
 
   //contact
-  contact0: 9900,
-  contact1: 10100,
-  contact2: 10300,
+  contact0: 10600,
+  contact1: 10800,
+  contact2: 11000,
 
   //nav
-  nav0: 10500,
-  nav1: 10700,
-  nav2: 10900,
-  nav3: 11100,
-  nav4: 11300,
-  nav5: 11500,
-  nav6: 11700,
+  nav0: 11400,
+  nav1: 11600,
+  nav2: 11800,
+  nav3: 12000,
+  nav4: 12200,
+  nav5: 12400,
+  nav6: 12600,
 
   //skills
-  skill0: 12000,
-  skill1: 12100,
-  skill2: 12200,
-  skill3: 12300,
-  skill4: 12400,
-  skill5: 12500,
-  skill6: 12600,
-  skill7: 12700,
-  skill8: 12800,
-  skill9: 12900,
-  skill10: 13000,
-  skill11: 13100,
-  skill12: 13200,
-  skill13: 13300,
-  skill14: 13400,
+  skill0: 13000,
+  skill1: 13100,
+  skill2: 13200,
+  skill3: 13300,
+  skill4: 13400,
+  skill5: 13500,
+  skill6: 13600,
+  skill7: 13700,
+  skill8: 13800,
+  skill9: 13900,
+  skill10: 14000,
+  skill11: 14100,
+  skill12: 14200,
+  skill13: 14300,
+  skill14: 14400,
 
   //content
-  content: 13700,
+  content: 14700,
 };
